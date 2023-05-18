fea62db72c73117cfe730c8d4e7b6b914b98f7ea
TestLeaf <babu@testleaf.com>
2023-05-18 14:29:22 +0530

    Updated the checks

--- health-check.sh
+++ health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuration
-THRESHOLD_CPU=80
+THRESHOLD_CPU=100
 THRESHOLD_MEM=80
 THRESHOLD_DISK=80
 EMAIL="babu@testleaf.com"
@@ -11,6 +11,9 @@ DB_USERNAME="bn_opencart"
 DB_PASSWORD="fe390080a5573e5bbfb0ca29b926fa5529ea60020aec11c5979354f3c128f7b4"
 DB_DATABASE="bitnami_opencart"
 DB_PORT=3306
+
+# Track if any checks fail
+checks_passed=true
 
 # Check CPU usage
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | \
@@ -23,6 +26,7 @@ echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%"
 if (( $(echo "$cpu_usage > $THRESHOLD_CPU" |bc -l) )); then
   echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%" | \
   mail -s "High CPU usage alert" $EMAIL
+  checks_passed=false
 fi
 
 # Check Memory usage
@@ -33,6 +37,7 @@ echo "Memory Usage: $mem_usage"
 if (( $(echo "$mem_usage > $THRESHOLD_MEM" |bc -l) )); then
   echo "Memory usage is higher than $THRESHOLD_MEM%, it's $mem_usage%" | \
   mail -s "High Memory usage alert" $EMAIL
+  checks_passed=false
 fi
 
 # Check Disk space usage
@@ -43,6 +48,7 @@ echo "Disk Usage: $disk_usage"
 if (( $(echo "$disk_usage > $THRESHOLD_DISK" |bc -l) )); then
   echo "Disk usage is higher than $THRESHOLD_DISK%, it's $disk_usage%" | \
   mail -s "High Disk usage alert" $EMAIL
+  checks_passed=false
 fi
 
 # Check status of Bitnami services
@@ -53,10 +59,19 @@ echo "Service Status: $services_status"
 if echo $services_status | grep -q "not running"; then
   echo "One or more Bitnami services are not running: $services_status" | \
   mail -s "Bitnami service status alert" $EMAIL
+  checks_passed=false
 fi
 
 # Check MySQL status
 if ! mysqladmin -h $DB_HOSTNAME -P $DB_PORT -u $DB_USERNAME -p$DB_PASSWORD status > /dev/null; then
   echo "MySQL is not running" | \
   mail -s "MySQL is down alert" $EMAIL
+  checks_passed=false
+fi
+
+# If all checks passed, run the tests
+if $checks_passed; then
+  echo "All checks passed"
+else
+  echo "Some checks did not pass"
 fi
