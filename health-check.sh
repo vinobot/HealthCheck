f9f5ef5900e8e039ec0e8cf8c6a5fa7baacd1916
TestLeaf <babu@testleaf.com>
2023-05-20 18:34:56 +0530

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -17,8 +17,7 @@ checks_passed=true
 
 # Check CPU usage
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | \
-           sed "s/.*, *\([0.9.]*\)%* id.*/\1/" | \
-           awk '{print 100 - $1}')
+           awk '{print $7}' | sed "s/ni[,]//)
 
 echo "CPU Usage: $cpu_usage"
 echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%"
